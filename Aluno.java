2df23c77fc0fd7a09215b6932959d079238fa69d
Gabriel Pontes <pontesgabriel174@gmail.com>
2023-08-15 19:32:27 +0000

    Exemplo de aula

---
+++ Aluno.java
@@ -0,0 +1,16 @@
+import javax.print.DocFlavor.STRING;
+
+public class Aluno{
+    private String nome;
+    private int idade;
+
+    public void setNome(String nome){
+        this.nome = nome;
+
+    }
+    public String getNome(){
+        return nome;
+    }
+   
+    
+}
